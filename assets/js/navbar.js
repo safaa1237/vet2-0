9d8b0de28fc5b7b4d85f797b7740a0ddfbc1978c
0yasum1 <36842990+0yasum1@users.noreply.github.com>
2020-08-10 16:05:13 +0200

    new nav bar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,20 @@
-function navbarFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+const navSlide = () => {
+  const burger = document.getElementsByClassName('.burger');
+  const nav = document.getElementsByClassName('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  //toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle('nav-active');
+  });
+
+  
+
+  //animate links
+  navLinks.forEach((link, index) => {
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index /7}s`;
+    console.log(index / 5 + 0.2);
+  });
+}
+
+navSlide();
