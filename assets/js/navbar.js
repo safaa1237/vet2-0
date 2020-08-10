f7976a859954e9b994aa1a5e3012dbc3cff7a6f6
0yasum1 <36842990+0yasum1@users.noreply.github.com>
2020-08-10 16:28:51 +0200

    working nav bar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,19 +1,24 @@
 const navSlide = () => {
-  const burger = document.getElementsByClassName('.burger');
-  const nav = document.getElementsByClassName('.nav-links');
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
 
   //toggle nav
   burger.addEventListener("click", () => {
     nav.classList.toggle('nav-active');
-  });
 
-  
+      //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation){
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7}s`;
+      }
+    });
 
-  //animate links
-  navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwards ${index /7}s`;
-    console.log(index / 5 + 0.2);
+    //burger animation
+    burger.classList.toggle('toggle');
+
   });
 }
 
