545a31bff67ba1e6495b7148052e13f7f802ebfe
0yasum1 <36842990+0yasum1@users.noreply.github.com>
2020-09-10 10:31:45 +0200

    Update img gallery

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -22,4 +22,31 @@ const navSlide = () => {
   });
 }
 
+const imgPreview = () => {
+  const modal = document.querySelector(".modal-img");
+  const previews = document.querySelectorAll(".image-gallery img");
+  const original = document.querySelector(".full-img");
+  const imgText = document.querySelector(".caption");
+
+  previews.forEach((preview) => {
+    preview.addEventListener("click", () => {
+      modal.classList.add("open");
+      original.classList.add("open");
+      //dynamic change text and image
+      const originalSrc = preview.getAttribute("data-original");
+      original.src = `assets/img/${originalSrc}`;
+      const altText = preview.alt;
+      imgText.textContent = altText;
+    });
+  });
+
+  modal.addEventListener("click", (e) => {
+    console.log(e);
+    if (e.target.classList.contains("modal-img")) {
+      modal.classList.remove("open");
+    }
+  });
+}
+
+imgPreview();
 navSlide();
