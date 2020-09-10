454cbd5f0586aecca15d2814c716e00733e82c62
0yasum1 <36842990+0yasum1@users.noreply.github.com>
2020-09-10 09:26:59 +0200

    Update img gallery

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function navbarFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,3 +7,44 @@ function navbarFunction() {
       x.className = "topnav";
     }
   }
+=======
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  //toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle('nav-active');
+
+      //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation){
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7}s`;
+      }
+    });
+
+    //burger animation
+    burger.classList.toggle('toggle');
+
+  });
+}
+
+const imgPreview = () => {
+  const modal = document.querySelector(".modal");
+  const previews = document.querySelectorAll(".gallery a");
+  const original = document.querySelector(".full-img");
+  const imgText = document.querySelector(".caption");
+
+  previews.forEach(preview => {
+    preview.addEventListener('click', () => {
+      modal.classList.add("open");
+    })
+  })
+}
+
+imgPreview();
+navSlide();
+>>>>>>> Stashed changes
